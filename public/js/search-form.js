fafbe6b81147d2d5f65e94d019bf3bc278a40512
CodeMachine <98046355+CodeforMood@users.noreply.github.com>
2023-12-20 07:05:59 +0000

    Delete search-form.js

--- public/js/search-form.js
+++
@@ -1,7 +0,0 @@
-const inputFormElem = document.getElementById("search");
-const formElem = document.getElementById("navbar-form");
-
-formElem.addEventListener("submit", (ev) => {
-  ev.preventDefault();
-  fetch(`/?search=${inputFormElem.value}`);
-});
